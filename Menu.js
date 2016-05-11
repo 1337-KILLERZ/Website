95a51474c7ed497619cc73da0b8cd417d797d740
Reece Dunstan <reece.d@hotmail.co.uk>
2016-05-11 21:47:50 +0100

    More Pages

--- Menu.js
+++ Menu.js
@@ -1,10 +1,8 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
@@ -17,4 +15,4 @@ window.onclick = function(event) {
       }
     }
   }
-}// JavaScript Document
+}
